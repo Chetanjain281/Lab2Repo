78dd0811a0691a281c21caa5381a1041240ff599
Chetan Jain <113141484+Chetanjain281@users.noreply.github.com>
2025-06-05 11:02:55 +0530

    Update Classes.java

--- Classes.java
+++ Classes.java
@@ -1,7 +1,34 @@
-public class Classes{
+import java.util.Scanner;
 
+class Student {
+    private String name;
+    private int id;
 
-public static void main(String[] args) {
-    System.out.println("hi");
+    public Student(String name, int id) {
+        this.name = name;
+        this.id = id;
+    }
+
+    public void printDetails() {
+        System.out.println("Student Name: " + name + ", ID: " + id);
+    }
 }
+
+public class Classes {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Get student info from user
+        System.out.print("Enter student name: ");
+        String name = scanner.nextLine();
+
+        System.out.print("Enter student ID: ");
+        int id = scanner.nextInt();
+
+        Student student = new Student(name, id);
+        student.printDetails();
+
+        scanner.close();
+    }
 }
